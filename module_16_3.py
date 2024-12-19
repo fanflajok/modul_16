b67188531e70697d0aaeaf9b870ef6a1b190cd76
Victor <fanflajok@gmail.com>
2024-12-19 20:34:20 +0300

    Add files via upload
    Цель: выработать навык работы с CRUD запросами.
    
    
    Задача "Имитация работы с БД":
    
    Создайте новое приложение FastAPI и сделайте CRUD запросы.
    Создайте словарь users = {'1': 'Имя: Example, возраст: 18'}
    Реализуйте 4 CRUD запроса:
    
    get запрос по маршруту '/users', который возвращает словарь users.
    post запрос по маршруту '/user/{username}/{age}', который добавляет в словарь по максимальному по значению ключом значение строки "Имя: {username}, возраст: {age}". И возвращает строку "User <user_id> is registered".
    put запрос по маршруту '/user/{user_id}/{username}/{age}', который обновляет значение из словаря users под ключом user_id на строку "Имя: {username}, возраст: {age}". И возвращает строку "The user <user_id> is updated"
    delete запрос по маршруту '/user/{user_id}', который удаляет из словаря users по ключу user_id пару.
    Выполните каждый из этих запросов по порядку. Ответы должны совпадать:
    
    1. GET '/users'
    {
    "1": "Имя: Example, возраст: 18"
    }
    2. POST '/user/{username}/{age}' # username - UrbanUser, age - 24
    "User 2 is registered"
    3. POST '/user/{username}/{age}' # username - NewUser, age - 22
    "User 3 is registered"
    4. PUT '/user/{user_id}/{username}/{age}' # user_id - 1, username - UrbanProfi, age - 28
    "User 1 has been updated"
    5. DELETE '/user/{user_id}' # user_id - 2
    "User 2 has been deleted"
    6. GET '/users'
    {
    "1": "Имя: UrbanProfi, возраст: 28",
    "3": "Имя: NewUser, возраст: 22"
    }

---
+++ module_16_3.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Path
+app = FastAPI()
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get("/")
+async def get_all_records():
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def add_data(username: str = Path(description='Enter Name', example='Victor'),
+                   age: int = Path(ge=18, le=120, description='Enter age', example='32')):
+    curr_index = str(int(max(users,key=int)) + 1)
+    users[curr_index] = f'Имя: {username}, возраст: {age}'
+    return f'User {curr_index} is registered'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_data(user_id: int = Path(description='Enter ID',example='3'),
+                      username: str = Path(description='Enter Name', example='Victor'),
+                      age: int = Path(ge=18, le=120, description='Enter age', example='32')):
+    users[str(user_id)] = f'Имя: {username}, возраст: {age}'
+    return f'The user {user_id} is updated'
+
+
+@app.post('/user/{user_id}')
+async def delete_data(user_id: int):
+    users.pop(str(user_id))
+    return users
+
+
